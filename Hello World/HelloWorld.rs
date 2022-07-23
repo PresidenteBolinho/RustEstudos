35d9c4b1a8a6d63c4f513c87a02075666d35c3f0
Gustavo Pinheiro <gustavospr@hotmail.com>
2022-07-23 15:34:53 -0300

    Criado um Hello World

---
+++ Hello World/HelloWorld.rs
@@ -0,0 +1,10 @@
+// Está aqui, é a função principal do Rust
+fn main() {
+    // As declarações aqui são executadas quando o compilador binario é chamado
+
+    // Mostra o texto no console.
+    println!("Hello World!");
+
+    // Activity: Retornar no console a frase("I'm a Rustacean!")
+    println!("I'm a Rustacean!");
+}
